b67aa7d97df9e7ab04b71b05b684e68d4f1c1eac
solemnify2 <50224420+solemnify2@users.noreply.github.com>
2024-06-19 17:56:26 +0900

    Update GestureMR4BMSGUI.py

--- GestureMR4BMSGUI.py
+++ GestureMR4BMSGUI.py
@@ -9,61 +9,110 @@ from PIL import Image, ImageTk, ImageDraw
 import pystray
 import time
 
-# Initialize MediaPipe Hands
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-mp_drawing = mp.solutions.drawing_utils
 
-# Initialize keyboard controller
-keyboard = Controller()
+class GestureMR4BMSApp:
 
-# Flags to control the loop
-running = False
-show_feed = False
-tray_start_stop = ""
+    def __init__(self, root):
+        self.root = root
 
-# Variable for threshold
-threshold_x = 0
-threshold_y = 0.5
-detection_mode = 0
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        self.mp_drawing = mp.solutions.drawing_utils
 
-MR_WATERMARK = 5   # MR Cover On Watermark
-mr_cover_watermark = 0
+        # Initialize keyboard controller
+        self.keyboard = Controller()
 
-# Function to toggle MR cover (replace with actual implementation)
-def mr_cover_on():
-    keyboard.press(Key.shift)
-    keyboard.press('1')
-    keyboard.release('1')
-    keyboard.release(Key.shift)
+        # Flags to control the loop
+        self.running = False
+        self.show_feed = False
+        self.tray_start_stop = ""
 
-def mr_cover_off():
-    keyboard.press(Key.shift)
-    keyboard.press('1')
-    keyboard.release('1')
-    keyboard.release(Key.shift)
+        # Variable for threshold
+        self.threshold_x = 0
+        self.threshold_y = 0.5
+        self.detection_mode = 0
 
-def detect_hand():
-    global running, show_feed, threshold_y, mr_cover_watermark
-    global toogle_feed_var, detection_mode
+        self.MR_WATERMARK = 5   # MR Cover On Watermark
+        self.mr_cover_watermark = 0
+        
+        self.cap = cv2.VideoCapture(0)
+        
+        self.label = tk.Label(root)
+        self.label.pack()
+        
+        root.title(f"GestureMR4BMS")
 
-    print("detection_thread enter")        
-    # Initialize the webcam
-    cap = cv2.VideoCapture(0)
+        # Style
+        style = ttk.Style()
+        style.configure("TButton", padding=2)
+        style.map("TButton", foreground=[('pressed', 'white'), ('active', 'blue')],
+                          background=[('pressed', 'blue'), ('active', 'lightblue')])
 
-    print("detection_thread loop enter")
+        # Create a frame to contain the buttons and toggle switch
+        self.button_frame = tk.Frame(root)
+        self.button_frame.pack(pady=5)
 
-    while running and cap.isOpened():
-        ret, frame = cap.read()
+        # Create buttons for the main window
+        self.start_stop_button = ttk.Button(self.button_frame, text="\u25B6", width=2, command=self.on_start_detection, style="TButton")
+        self.start_stop_button.pack(side=tk.LEFT, padx=5)
+
+        self.threshold_label = tk.Label(self.button_frame, text="Detect:")
+        self.threshold_label.pack(side=tk.LEFT, padx=5)
+
+        # Create a StringVar to hold the value of the selected option
+        self.option_var = tk.StringVar()
+
+        # Create a Combobox with detection modes
+        options = ["wrist only", "any parts of hand", "all parts of hand"]
+        self.combobox = ttk.Combobox(self.button_frame, textvariable=self.option_var, values=options, state='readonly')
+        self.combobox.current(0)  # Set the default value to the first option
+        self.combobox.bind("<<ComboboxSelected>>", self.on_option_select)
+        self.combobox.pack(side=tk.LEFT, padx=5)
+
+        self.mode_label = tk.Label(self.button_frame, text="witin Area:")
+        self.mode_label.pack(side=tk.LEFT, padx=5)
+
+        self.threshold_slider = tk.Scale(self.button_frame, from_=0, to=100, orient=tk.HORIZONTAL, command=self.on_update_threshold, showvalue=False)
+        self.threshold_slider.set(self.threshold_y * 100)
+        self.threshold_slider.pack(side=tk.LEFT, padx=5)
+
+        self.toggle_feed_var = tk.BooleanVar()
+        self.toggle_feed_switch = tk.Checkbutton(self.button_frame, text="View Webcam Feed", variable=self.toggle_feed_var, command=self.on_update_feed)
+        self.toggle_feed_switch.pack(side=tk.LEFT, padx=5)
+
+        self.button_frame.pack(side=tk.TOP, fill=tk.X)
+
+        self.video_label = tk.Label(root)
+        self.video_label.pack(pady=10)
+            
+        
+    # Function to toggle MR cover (replace with actual implementation)
+    def mr_cover_on():
+        keyboard.press(Key.shift)
+        keyboard.press('1')
+        keyboard.release('1')
+        keyboard.release(Key.shift)
+
+    def mr_cover_off():
+        keyboard.press(Key.shift)
+        keyboard.press('1')
+        keyboard.release('1')
+        keyboard.release(Key.shift)
+
+    def detect_hand(self):
+        global running, show_feed, threshold_y, mr_cover_watermark
+        global toogle_feed_var, detection_mode
+
+        ret, frame = self.cap.read()
         if not ret:
-            break
+            return
 
         # Get frame dimensions
         frame_height, frame_width, _ = frame.shape
 
         # Process the frame and find hands
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        result = hands.process(rgb_frame)
+        result = self.hands.process(rgb_frame)
 
         landmarks = 0       # the number of hands detected
 
@@ -73,203 +122,140 @@ def detect_hand():
                 wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
                 # detectio option) Select one of the followings that you prefer
                 # Opt1) Check if the whole hand is in the specified area of the screen
-                if (detection_mode == 0 and wrist.x > threshold_x and wrist.x < 1 - threshold_x and wrist.y > threshold_y) or \
-                   (detection_mode == 1 and any(landmark.x > threshold_x and landmark.x < 1 - threshold_x and landmark.y > threshold_y for landmark in hand_landmarks.landmark)) or \
-                   (detection_mode == 2 and all(landmark.x > threshold_x and landmark.x < 1 - threshold_x and landmark.y > threshold_y for landmark in hand_landmarks.landmark)):
+                if (self.detection_mode == 0 and wrist.x > self.threshold_x and wrist.x < 1 - self.threshold_x and wrist.y > self.threshold_y) or \
+                   (self.detection_mode == 1 and any(landmark.x > self.threshold_x and landmark.x < 1 - self.threshold_x and landmark.y > self.threshold_y for landmark in hand_landmarks.landmark)) or \
+                   (self.detection_mode == 2 and all(landmark.x > self.threshold_x and landmark.x < 1 - self.threshold_x and landmark.y > self.threshold_y for landmark in hand_landmarks.landmark)):
                     landmarks = landmarks + 1
-                    if mr_cover_watermark == 0:                   # Immediate mr_cover_watermark on as soon as any hands detected
+                    if self.mr_cover_watermark == 0:                   # Immediate mr_cover_watermark on as soon as any hands detected
                         mr_cover_on()
-                        video_label.config(text=f"Hand detected at x: {wrist.x:.2f}, y: {wrist.y:.2f}, z: {wrist.z:.2f}", image='')
+                        self.video_label.config(text=f"Hand detected at x: {wrist.x:.2f}, y: {wrist.y:.2f}, z: {wrist.z:.2f}", image='')
                         
-                    mr_cover_watermark = MR_WATERMARK             # high watermark immediately, when any hands detected
+                    self.mr_cover_watermark = MR_WATERMARK             # high watermark immediately, when any hands detected
 
-                    if show_feed:
+                    if self.show_feed:
                         mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 
         # If no hands detected or outside detection region
         if landmarks == 0: 
-            mr_cover_watermark = max(mr_cover_watermark - 1,0)    # Count-down slowly
+            self.mr_cover_watermark = max(self.mr_cover_watermark - 1,0)    # Count-down slowly
 
             # mr_cover_watermark off after some delay to prevent hand detection noise
-            if mr_cover_watermark == 1:
+            if self.mr_cover_watermark == 1:
                 mr_cover_off()
-                video_label.config(text="No hand detected", image='')
+                self.video_label.config(text="No hand detected", image='')
 
-        if show_feed:
+        if self.show_feed:
             # Draw detection area on the frame
-            cv2.rectangle(frame, (0, int(threshold_y * frame_height)), (frame_width, frame_height), (0, 255, 0), 3)
+            cv2.rectangle(frame, (0, int(self.threshold_y * frame_height)), (frame_width, frame_height), (0, 255, 0), 3)
 
             # Update the Tkinter label with the new frame
             img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             imgtk = ImageTk.PhotoImage(image=img)
-            video_label.imgtk = imgtk
-            video_label.configure(image=imgtk)
+            self.video_label.imgtk = imgtk
+            self.video_label.configure(image=imgtk)
             
-        time.sleep(0.2)  # Slow down
+        if self.running:
+            self.root.after(20, self.detect_hand)
 
-    print("detection_thread loop exit")
+    def on_start_detection(self):
+        if self.running == False:
+            self.running = True
 
-    cap.release()
-    cv2.destroyAllWindows()
-    
-    print("detection_thread exit")
+            self.start_stop_button.config(text="\u25A0", command=self.on_stop_detection)
+            self.video_label.config(text="Detecting", image='')
 
-def start_detection():
-    global running, detection_thread
+            self.detect_hand()        
 
-    if running == False:
-        running = True
-        detection_thread = threading.Thread(target=detect_hand)
-        detection_thread.start()
+    def on_stop_detection(self):
+        if self.running == True:
+            self.running = False
+
+            self.video_label.config(text="Stopped", image='')
+            self.start_stop_button.config(text="\u25B6", command=self.on_start_detection)
+
+    def create_image(self):
+        # Generate an image to use as the icon
+        width = 64
+        height = 64
+        image = Image.new('RGB', (width, height), "white")
+        dc = ImageDraw.Draw(image)
+        dc.rectangle((width // 4, height // 4, 3 * width // 4, 3 * height // 4), fill="black")
+        return image
+
+    def on_tray_start_stop(self, item):
+       
+        if self.running == False:
+            self.on_start_detection()
+            self.tray_start_stop = "\u25A0 Stop"
+        else:
+            self.on_stop_detection()
+            self.tray_start_stop = "\u25B6 Start"
+
+    def on_tray_quit(self, item):
+        self.on_stop_detection()
+
+        self.icon.stop()
+
+        self.root.deiconify()
+        self.root.quit()  # Properly exit the Tkinter main loop
+
+    def on_show_window(self, item):
+        self.icon.stop()
         
-        start_stop_button.config(text="\u25A0", command=stop_detection)
+        self.root.after(0, self.root.deiconify)
+
+    def on_hide_window(self, event=None):
+        global tray_start_stop, running
         
-        video_label.config(text="Detecting", image='')
-
-def stop_detection():
-    global running, detection_thread
-    
-    if running == True:
-        running = False
+        if self.root.state() == 'withdrawn':
+            return
+            
+        self.root.withdraw()
         
-        print("joining")
-        detection_thread.join()
-        print("joined") 
+        if self.running == False:
+            self.tray_start_stop = "\u25B6 Start"
+        else:
+            self.tray_start_stop = "\u25A0 Stop"
+
+        self.tray_menu = pystray.Menu(
+            pystray.MenuItem(lambda text: self.tray_start_stop, self.on_tray_start_stop),
+            pystray.MenuItem('Show', self.on_show_window),
+            pystray.MenuItem('About', self.on_show_about),
+            pystray.MenuItem('Quit', self.on_tray_quit))
+            
+        image = self.create_image()
+        self.icon = pystray.Icon("GestureMR4BMS", image, "GestureMR4BMS", self.tray_menu)
         
-        start_stop_button.config(text="\u25B6", command=start_detection)
-        video_label.config(text="Stopped. Click \u25B6 to start detecting.", image='')
+        threading.Thread(target=self.icon.run).start()
 
-def create_image():
-    # Generate an image to use as the icon
-    width = 64
-    height = 64
-    image = Image.new('RGB', (width, height), "white")
-    dc = ImageDraw.Draw(image)
-    dc.rectangle((width // 4, height // 4, 3 * width // 4, 3 * height // 4), fill="black")
-    return image
+    def on_update_threshold(self, value):
+        self.threshold_y = float(value) / 100
 
-def on_tray_start_stop(icon, item):
-    global running, tray_start_stop
-    
-    if running == False:
-        start_detection()
-        tray_start_stop = "\u25A0 Stop"
-    else:
-        stop_detection()
-        tray_start_stop = "\u25B6 Start"
+    def on_update_feed(self):
+        self.show_feed = self.toggle_feed_var.get()
+        self.video_label.config(text="", image="")
 
-def on_tray_quit(icon, item):
-    stop_detection()
-
-    icon.stop()
-
-    root.deiconify()
-    root.quit()  # Properly exit the Tkinter main loop
-
-def show_window(icon, item):
-    icon.stop()
-    
-#    root.after(0, root.deiconify)
-    root.deiconify()
-
-def hide_window(event=None):
-    global tray_start_stop, running
-    
-    if root.state() == 'withdrawn':
-        return
+    # Function to handle the selection of an option
+    def on_option_select(self, event):
+        self.detection_mode = self.combobox['values'].index(self.combobox.get())
         
-    root.withdraw()
-    
-    if running == False:
-        tray_start_stop = "\u25B6 Start"
-    else:
-        tray_start_stop = "\u25A0 Stop"
+    def on_quit_program(self):
+        self.on_stop_detection()
 
-    tray_menu = pystray.Menu(
-        pystray.MenuItem(lambda text: tray_start_stop, on_tray_start_stop),
-        pystray.MenuItem('Show', show_window),
-        pystray.MenuItem('About', show_about),
-        pystray.MenuItem('Quit', on_tray_quit))
-        
-    image = create_image()
-    icon = pystray.Icon("GestureMR4BMS", image, "GestureMR4BMS", tray_menu)
-    threading.Thread(target=icon.run).start()
+        print("quit")
+        self.root.quit()
 
-def update_threshold(value):
-    global threshold_y
-    threshold_y = float(value) / 100
+    def on_show_about(self):
+        messagebox.showinfo("About", "GestureMR4BMS Version 0.2.0\n\nCopyright (C) 2024 Hong Yeon Kim\n\nFor more information, visit: https://github.com/solemnify2/GestureMR4BMS")
 
-def update_feed():
-    global show_feed
-    show_feed = toggle_feed_var.get()
-    video_label.config(text="", image='')
+if __name__ == "__main__":
+    # Tkinter GUI setup
+    root = tk.Tk()
 
-# Function to handle the selection of an option
-def on_option_select(event):
-    global detection_mode
-    
-    detection_mode = combobox['values'].index(combobox.get())
-    
-def quit_program():
-    stop_detection()
+    app = GestureMR4BMSApp(root)
 
-    print("quit")
-    root.quit()
 
-def show_about():
-    messagebox.showinfo("About", "GestureMR4BMS Version 0.1.0\n\nCopyright (C) 2024 Hong Yeon Kim\n\nFor more information, visit: https://github.com/solemnify2/GestureMR4BMS")
+    root.protocol('WM_DELETE_WINDOW', app.on_quit_program)
+    root.bind("<Unmap>", app.on_hide_window)
 
-# Tkinter GUI setup
-root = tk.Tk()
-root.title(f"GestureMR4BMS")
-
-root.protocol('WM_DELETE_WINDOW', quit_program)
-
-root.bind("<Unmap>", hide_window)
-
-# Style
-style = ttk.Style()
-style.configure("TButton", padding=2)
-style.map("TButton", foreground=[('pressed', 'white'), ('active', 'blue')],
-                  background=[('pressed', 'blue'), ('active', 'lightblue')])
-
-# Create a frame to contain the buttons and toggle switch
-button_frame = tk.Frame(root)
-button_frame.pack(pady=5)
-
-# Create buttons for the main window
-start_stop_button = ttk.Button(button_frame, text="\u25B6", width=2, command=start_detection, style="TButton")
-start_stop_button.pack(side=tk.LEFT, padx=5)
-
-threshold_label = tk.Label(button_frame, text="Detect:")
-threshold_label.pack(side=tk.LEFT, padx=5)
-
-# Create a StringVar to hold the value of the selected option
-option_var = tk.StringVar()
-
-# Create a Combobox with detection modes
-options = ["wrist only", "any parts of hand", "all parts of hand"]
-combobox = ttk.Combobox(button_frame, textvariable=option_var, values=options, state='readonly')
-combobox.current(0)  # Set the default value to the first option
-combobox.bind("<<ComboboxSelected>>", on_option_select)
-combobox.pack(side=tk.LEFT, padx=5)
-
-mode_label = tk.Label(button_frame, text="witin Area:")
-mode_label.pack(side=tk.LEFT, padx=5)
-
-threshold_slider = tk.Scale(button_frame, from_=0, to=100, orient=tk.HORIZONTAL, command=update_threshold, showvalue=False)
-threshold_slider.set(threshold_y * 100)
-threshold_slider.pack(side=tk.LEFT, padx=5)
-
-toggle_feed_var = tk.BooleanVar()
-toggle_feed_switch = tk.Checkbutton(button_frame, text="View Webcam Feed", variable=toggle_feed_var, command=update_feed)
-toggle_feed_switch.pack(side=tk.LEFT, padx=5)
-
-button_frame.pack(side=tk.TOP, fill=tk.X)
-
-video_label = tk.Label(root)
-video_label.pack(pady=10)
-
-start_detection()
-
-root.mainloop()
+    root.mainloop()
